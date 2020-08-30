5a76ec9b108fc079bc02c181dc3a170f6edfa9f8
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2020-08-30 12:01:48 +0200

    nicer colours

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -20,19 +20,23 @@ const Theme = ({ children }) => {
       createMuiTheme({
         palette: {
           type: prefersDarkMode ? 'dark' : 'light',
+          background: {
+            default: prefersDarkMode ? '#15232d' : '#efefef',
+            paper: prefersDarkMode ? '#193549' : '#fff',
+          },
           primary: {
-            light: 'hsla(217, 100%, 61%, 0.30)',
-            main: '#3a86ff',
+            light: 'hsla(212, 72%, 34%, 0.30)',
+            main: '#185294',
           },
           secondary: {
-            main: '#ffbe0b',
+            main: '#ff9d00',
           },
           success: {
-            light: 'hsla(265, 83%, 57%, 0.30)',
-            main: '#8338ec',
+            light: 'hsla(212, 72%, 34%, 0.30)',
+            main: '#185294',
           },
           error: {
-            main: '#ff006e',
+            main: '#A22929',
           },
           warning: {
             main: '#fb5607',
